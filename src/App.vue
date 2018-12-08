eeb39ff0f3f4b2c00fd0a7ec84de595536323ef3
huanyu2015 <shuyunfengcsdn@gmail.com>
2018-12-08 21:13:02 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,21 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
+<!--
+路由就是根据URL的不同返回给用户不同的内容
+-->
--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 移动端点击事件300毫秒延迟问题的解决:安装fastclick文件，并添加下面的语句。
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
+  router: router,
+  components: { App: App },
   template: '<App/>'
 })
