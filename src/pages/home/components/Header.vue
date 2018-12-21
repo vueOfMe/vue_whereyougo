93ed84d7239283be690f76deab5d2442c921739d
huanyu2015 <shuyunfengcsdn@gmail.com>
2018-12-21 22:29:09 +0800

    add city header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 
 </template>
@@ -31,7 +33,7 @@
   // 1rem = html font size = 50px
   .header {
     display: flex;
-    line-height: .86rem;
+    line-height: $headerHeight
     background: $bgColor;
     color: #ffffff
   }
@@ -61,7 +63,8 @@
   .header-right {
     width: 1.28rem;
     float: right;
-    text-align: center
+    text-align: center;
+    color: #ffffff
   }
 
   .arrow-icon {
